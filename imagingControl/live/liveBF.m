134fe5bec307d2a1fe1306500c2321de97fdf9ea
Maria Mukhina <mmuxina@gmail.com>
2022-11-30 14:45:15 +0000

    Update liveBF.m

--- imagingControl/live/liveBF.m
+++ imagingControl/live/liveBF.m
@@ -1,7 +1,6 @@
 function [] = liveBF(varargin)
 %LIVEBF runs imaging in the BF channel in a current focal plane with real-time display, data are not saved
-% BF channel can be either preselected with setupChannel or specified by
-% the arguments in varargin: liveBF('GreenBrightField',50)
+% BF channel can be either preselected with setupChannel or specified as function input: liveBF('GreenBrightField',50)
 
 if ~isempty(varargin)
    channel =  varargin{1};
